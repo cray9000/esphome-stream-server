a521d673a235e9fc19bf36a0c5189b1e6dbbadcf
cray9000 <65761829+cray9000@users.noreply.github.com>
2024-11-05 20:33:52 +0100

    Update stream_server.cpp

--- components/stream_server/stream_server.cpp
+++ components/stream_server/stream_server.cpp
@@ -94,28 +94,9 @@ void StreamServerComponent::cleanup() {
 void StreamServerComponent::read() {
     size_t len = 0;
     int available;
-    while ((available = this->stream_->available()) > 0) {
-        size_t free = this->buf_size_ - (this->buf_head_ - this->buf_tail_);
-        if (free == 0) {
-            // Only overwrite if nothing has been added yet, otherwise give flush() a chance to empty the buffer first.
-            if (len > 0)
-                return;
-
-            ESP_LOGE(TAG, "Incoming bytes available, but outgoing buffer is full: stream will be corrupted!");
-            free = std::min<size_t>(available, this->buf_size_);
-            this->buf_tail_ += free;
-            for (Client &client : this->clients_) {
-                if (client.position < this->buf_tail_) {
-                    ESP_LOGW(TAG, "Dropped %u pending bytes for client %s", this->buf_tail_ - client.position, client.identifier.c_str());
-                    client.position = this->buf_tail_;
-                }
-            }
-        }
-
-        // Fill all available contiguous space in the ring buffer.
-        len = std::min<size_t>(available, std::min<size_t>(this->buf_ahead(this->buf_head_), free));
-        this->stream_->read_array(&this->buf_[this->buf_index(this->buf_head_)], len);
-        this->buf_head_ += len;
+    // Handle reading from a different source, e.g., a different stream or input device
+    while ((available = this->socket_->available()) > 0) {
+        // Handle incoming data and buffer it, if necessary.
     }
 }
 
@@ -156,19 +137,17 @@ void StreamServerComponent::write() {
         if (client.disconnected)
             continue;
 
-        while ((read = client.socket->read(&buf, sizeof(buf))) > 0)
-            this->stream_->write_array(buf, read);
+        while ((read = client.socket->read(&buf, sizeof(buf))) > 0) {
+            // Handle the data (e.g., send it somewhere else)
+        }
 
         if (read == 0 || errno == ECONNRESET) {
             ESP_LOGD(TAG, "Client %s disconnected", client.identifier.c_str());
             client.disconnected = true;
-        } else if (errno == EWOULDBLOCK || errno == EAGAIN) {
-            // Expected if the (TCP) receive buffer is empty, nothing to do.
-        } else {
-            ESP_LOGW(TAG, "Failed to read from client %s with error %d!", client.identifier.c_str(), errno);
         }
     }
 }
+
 
 StreamServerComponent::Client::Client(std::unique_ptr<esphome::socket::Socket> socket, std::string identifier, size_t position)
     : socket(std::move(socket)), identifier{identifier}, position{position} {}
